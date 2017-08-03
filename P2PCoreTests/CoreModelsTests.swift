3b06be53fc504cc016b567270e3188764dcd8591
vitkuzmenko <kuzmenko.v.u@gmail.com>
2017-08-03 16:16:50 +0300

    models tests start

---
+++ P2PCoreTests/CoreModelsTests.swift
@@ -0,0 +1,26 @@
+//
+//  CoreModelsTests.swift
+//  P2P_iOS
+//
+//  Created by Vitaliy Kuzmenko on 03/08/2017.
+//  Copyright © 2017 Wallet One. All rights reserved.
+//
+
+import XCTest
+
+class CoreModelsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    func testDealModel() {
+        
+        
+        
+    }
+    
+}
+
+
